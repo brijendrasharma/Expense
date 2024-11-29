8d9bbe52cab5560a76696312dc99146c3fb20c17
Brijendra Sharma <brijendra.sharma2806@gmail.com>
2024-11-29 22:59:03 +0530

    adding category controller

---
+++ ExpenserAPI/Controllers/CategoryController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace ExpenserAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetCategories()
+        {
+            string categories = "This is to return Categories";
+            return Ok(categories);
+        }
+
+        [HttpPost]
+        [ProducesResponseType(400)]
+        [ProducesResponseType(201)]
+        public IActionResult AddCategory([FromBody] string category)
+        {
+            // Placeholder logic; Replace with database logic later
+            return CreatedAtAction(nameof(AddCategory), $"Ban gya bhai, {category}");
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetCategorybyId([FromRoute] int id)
+        {
+            return Ok();
+        }
+    }
+}
